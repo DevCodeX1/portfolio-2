2bccc88bc2670d08c6efa5bae13be3c977c04542
DevCodeX1 <countryboya20@gmail.com>
2024-07-17 14:15:50 -0500

    "Complete Blog WebSite version 1.0"

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ====================== Typing Animation ====================== */
+let typed = new Typed(".typing", {
+    strings:["","Web Designer...", "Web Developer...", "Full Stack Web Developer..."],
+    typeSpeed:90,
+    BackSpeed:60,
+    loop:true
+})
